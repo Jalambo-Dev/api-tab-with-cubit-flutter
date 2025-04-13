a7cf93b6bd64a09796637e846ba09aaf28ead99d
Jalambo-Dev <ahmedjalambo.dev@gmail.com>
2025-04-13 15:51:39 +0300

    Update fetchNewsByCategory method to include pagination support

--- lib/data/news_repo.dart
+++ lib/data/news_repo.dart
@@ -4,11 +4,15 @@ import 'package:tabbar_demo/data/news_service.dart';
 class NewsRepo {
   final NewsService _newsService = NewsService();
 
-  Future<List<NewsModel>> fetchNewsByCategory(String category) async {
-    return _newsService.getNewsByCategory(category).then((rawNews) {
-      return rawNews
-          .map<NewsModel>((json) => NewsModel.fromJson(json))
-          .toList();
-    });
+  Future<List<NewsModel>> fetchNewsByCategory(String category, int page) async {
+    try {
+      return _newsService.getNewsByCategory(category, page).then((rawNews) {
+        return rawNews
+            .map<NewsModel>((json) => NewsModel.fromJson(json))
+            .toList();
+      });
+    } catch (e) {
+      throw Exception('Failed to fetch news: $e');
+    }
   }
 }
--- lib/data/news_service.dart
+++ lib/data/news_service.dart
@@ -5,9 +5,9 @@ class NewsService {
   static const String _baseUrl = 'https://newsapi.org/v2/top-headlines';
   static const String _apiKey = '555c2576b0fc4f7f874703cb16e58319';
 
-  Future<List<dynamic>> getNewsByCategory(String category) async {
+  Future<List<dynamic>> getNewsByCategory(String category , int page) async {
     final uri =
-        Uri.parse("$_baseUrl?country=us&category=$category&apiKey=$_apiKey");
+        Uri.parse("$_baseUrl?country=us&category=$category&page=$page&apiKey=$_apiKey");
 
     final response = await http.get(uri);
 
